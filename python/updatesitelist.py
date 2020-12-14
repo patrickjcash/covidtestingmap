013553e85407927581f7e60a4a5e4d37f34b5ef0
patrickjcash <4813177+patrickjcash@users.noreply.github.com>
2020-12-14 15:04:46 -0500

    Add files via upload

--- python/updatesitelist.py
+++ python/updatesitelist.py
@@ -1,1 +1,109 @@
+ #! /usr/bin/python3
 
+import os
+import sys
+import requests
+import re
+import json
+import geojson
+from geojson import Feature, Point, FeatureCollection
+from bs4 import BeautifulSoup
+
+print("Starting...")
+
+# CHANGE current working directory to local folder where script resides
+
+if os.path.dirname(sys.argv[0]):
+	os.chdir(os.path.dirname(sys.argv[0]))
+
+
+# SET CONSTANTS
+
+API_KEY = 'YOUR_API_KEY'
+REQUEST_BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?'
+URL = 'https://www.nychealthandhospitals.org/covid-19-testing-sites/'
+ADDRESS_REGEX = '(.*)\n(.*)\s+(NY|New York)\s+(\d{5})'
+ADDRESS_REGEX_BACKUP = '(.*)(?:\nMonday)'
+BOUNDS = '40.9532067,-74.222055|40.5015477,-73.5560226'
+
+
+# PARSE WEBSITE AND UPDATE GEOJSON FILE
+
+print("Getting HTML data from NYC H+H site...")
+
+# parse html into soup
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+
+print("Parsing sites and addresses from HTML data...")
+
+# remove all a tags/links
+for a in soup.findAll('a'):
+	a.replaceWithChildren()
+site_candidates = soup.find_all('p',class_='m-b-20')
+
+sites = []
+
+for site_candidate in site_candidates:
+	# parse soup data 
+	name = site_candidate.get_text().partition('\n')[0].strip()
+	detail = str(site_candidate)
+
+	address_search = re.search(ADDRESS_REGEX,site_candidate.get_text())
+	if address_search is None:
+		address_search = re.search(ADDRESS_REGEX_BACKUP,site_candidate.get_text())
+
+	if address_search is None:
+		print("Couldn't find address for " + name)
+		continue
+	else:
+		address = ' '.join(address_search.groups())
+		site = {}
+		site['name'] = name
+		site['address'] = address
+		site['detail'] = detail
+		site['color'] = 'ltblue'
+		sites.append(site)
+		print("Found site: " + name)
+
+
+print("Geocoding addresses...")
+
+features = []
+
+for site in sites:
+	try:
+		# request geocoding data
+		payload = {'address' : site['address'], 'bounds' : BOUNDS, 'key' : API_KEY}
+		response = requests.get(REQUEST_BASE_URL, params=payload)
+	except:
+		print("Google Maps API request failed for " + site['name'])
+	else:
+		# parse Google Maps data
+		try:
+			json_data = response.json()
+			lat = json_data["results"][0]["geometry"]["location"]["lat"]
+			lng = json_data["results"][0]["geometry"]["location"]["lng"]
+
+			my_point = geojson.Point((lng, lat))
+			features.append(Feature(geometry=my_point, properties=site))
+			print("Retrieved lat/lng data for: " + site['name'])
+		except:
+			print("No lat/lng data returned for " + site['name'])
+
+
+collection = FeatureCollection(features)
+# print(collection)
+
+# Serializing json  
+json_object = json.dumps(collection, indent = 4)
+
+
+print("Exporting site data to JSON file...") 
+  
+# Writing to sample.json 
+with open("./sites.json", "w") as outfile:
+	outfile.write(json_object)
+
+print("Done!") 
---
+++ python/updatewaittimes.py
@@ -0,0 +1,132 @@
+ #! /usr/bin/python3
+
+import os
+import sys
+import requests
+import re
+import json
+import PyPDF2
+import csv
+
+print("Starting...")
+
+# CHANGE current working directory to local folder where script resides
+
+if os.path.dirname(sys.argv[0]):
+	os.chdir(os.path.dirname(sys.argv[0]))
+
+# SET CONSTANTS
+
+WAIT_URL = 'https://hhinternet.blob.core.windows.net/wait-times/testing-wait-times.pdf'
+WAIT_TIME_REGEX = '.*(?:\n.*){2}Last Reported Time:(?:\s*\d+:\d+\s*.M)?'
+
+
+# DOWNLOAD PDF
+
+print("Downloading wait times PDF...")
+
+response = requests.get(WAIT_URL)
+with open('./testing-wait-times.pdf', 'wb') as f:
+	f.write(response.content)
+
+
+# PARSE PDF
+
+# creating a pdf file object  
+pdfFileObj = open('testing-wait-times.pdf', 'rb')  
+# creating a pdf reader object  
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)  
+# creating a page object  
+pageObj = pdfReader.getPage(0)  
+# extracting text from page  
+text = pageObj.extractText()
+
+print("Scraping data from PDF...")
+
+# parse data
+raw_match_list = re.findall(WAIT_TIME_REGEX,text)
+sites_list = [item.split('\n') for item in raw_match_list]
+
+# create nested dictionary for later data lookups
+sites = {}
+for i in sites_list:
+	name = i[0]
+	wait = i[1]
+
+	if len(i) == 4:
+		reported = i[3]
+	else:
+		reported = "Not Reported Yet"
+
+	if wait.startswith('No Wait Time'):
+		color = 'green'
+	elif wait.startswith('0-30 Minutes'):
+		color = 'yellow'
+	elif wait.startswith('30-60 Minutes'):
+		color = 'yellow'
+	elif wait.startswith('1-1.5 Hours'):
+		color = 'orange'
+	elif wait.startswith('1.5-2 Hours'):
+		color = 'orange'
+	elif wait.startswith('More Than 2 Hours'):
+		color = 'red'
+	else:
+		color = 'ltblue'
+
+	sites[name] = {}
+	sites[name]['wait'] = wait
+	sites[name]['reported'] = reported
+	sites[name]['color'] = color
+
+# closing the pdf file object  
+pdfFileObj.close()
+
+# IMPORT MAPPING
+
+print("Importing PDF-website data mapping from CSV file...")
+
+namemap = {}
+
+with open('./namemap.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+         namemap[row['list_name']] = row['wait_name']
+
+# UPDATE site wait data
+
+print("Updating sites with PDF data...")
+
+
+with open('./sites.json') as f:
+	all_sites = json.load(f)
+
+for site in all_sites['features']:
+	site.pop('color',None)
+	site.pop('wait',None)
+	site.pop('reported',None)
+
+	name = site['properties']['name']
+
+	try:
+		site['properties']['color'] = sites[namemap[name]]['color']
+		site['properties']['wait'] = '<br/>Wait time: ' + sites[namemap[name]]['wait']
+		site['properties']['reported'] = '<br/>Reported time: ' + sites[namemap[name]]['reported']
+	except:
+		site['properties']['color'] = 'ltblue'
+		site['properties']['wait'] = ''
+		site['properties']['reported'] = ''
+
+print("Parsing data into geojson file...") 
+
+# Serializing json  
+json_object = json.dumps(all_sites, indent = 4)
+
+print("Writing geojson data to file...") 
+  
+# Writing to sample.json 
+with open("../cvmap.json", "w") as outfile:
+	outfile.write("cvfeed_callback(")
+	outfile.write(json_object)
+	outfile.write(");")
+
+print("Done!") 
